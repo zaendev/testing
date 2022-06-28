5e4318cf94897e9553650f7a5833b4985ec9b937
zain <31380073+zainstar@users.noreply.github.com>
2022-06-28 09:33:18 +0700

    Create time.php

---
+++ time.php
@@ -0,0 +1,12 @@
+<?php
+ 
+ function microtimeFloat()
+ {
+      list($usec, $sec) = explode(" ", microtime());
+      return ((float)$usec + (float)$sec);
+ }
+ 
+ $timeStar = microtimeFloat();
+ $timeEnd = microtimeFloat();
+ 
+ echo $timeEnd - $timeStar;
